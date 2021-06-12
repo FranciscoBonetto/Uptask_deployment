407afb6a294ca9c35535e5b7e5974d51f68cd595
Francisco Jose Bonetto <francisco.j.bonetto@gmail.com>
2021-06-12 01:46:28 -0300

    Actualizo port local

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ app.use('/',rutes());
 
 //Servidor y puerto
 const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, host, () => {
     console.log('El servidor esta funcionando');
